313b47a315df94c436dafcb4dc6831e02a51b6ea
sev7end <tornikequrdadze@gmail.com>
2021-12-22 13:51:44 +0400

    Fixed some issues with Db

--- NMCP/Startup.cs
+++ NMCP/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NMCP.Implementations.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,7 @@ namespace NMCP
         {
 
             services.AddControllers();
+            services.Add(new ServiceDescriptor(typeof(SqlConnectionManager), new SqlConnectionManager(Configuration.GetConnectionString("DefaultConnection"))));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NMCP", Version = "v1" });
